1b4bb7993e42e9f859ac9e086d01e738e6f7ea35
EllGuilherme <grds1000@gmail.com>
2022-04-01 21:40:00 -0300

    finalização da tarefa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int NumeroDigitado; 
+
+Console.Write("Digite um número: ");
+NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool NumeroEhPar = (NumeroDigitado % 2) ==0;
+
+if (NumeroEhPar)
+
+ {
+       Console.ForegroundColor = ConsoleColor.DarkYellow;
+       Console.WriteLine($"{NumeroDigitado} é par");
+ }
+
+else 
+{
+      Console.ForegroundColor = ConsoleColor.DarkMagenta;
+      Console.WriteLine($"{NumeroDigitado} é inpar");
+}
+
+Console.ResetColor();
